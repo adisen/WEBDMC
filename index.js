7b0fc22b6502ea878dcbe2243ecfd8cb5160a734
Israel Adetunji <adisen230@gmail.com>
2020-06-11 20:19:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,97 @@
+// Outputting
+// console.log("Hello World");
+// alert("Hello World");
+
+// Statements
+// Single line comment
+/*
+Muti line lines
+adjdjajd
+aidkdiakakl
+skdka
+sdjdjd
+sjdjdjda
+djdjdadadj
+*/
+
+// // var aalg = 4
+// console.log(5 + 9);
+// console.log("akakdkd");
+
+// // Variables
+// var x; // Variable Declaration
+// x = 7; // Variable assignment
+// var y;
+// y = 20;
+
+// let z = 67;
+// const Daniel = 78;
+
+// Daniel = 67;
+// console.log(z);
+
+// Operators
+
+// = assignment
+// + addition
+// console.log(`2 + 6 = ${2 + 6}`);
+// // - subtraction
+// console.log(`2 - 6 = ${2 - 6}`);
+// // * multiplication
+// console.log(`2 * 6 = ${2 * 6}`);
+// // / division
+// console.log(`6 + 2 = ${6 / 2}`);
+
+// % modulus (remainder)
+
+// Comparision operators
+// < less than
+// > greater than
+// == equal to
+// >= greater than or equal to
+// <= less than or equal
+
+// Data Types
+// Number - 8, 9, 9.7 (float)
+// var num = 89;
+// var flt = 98.677;
+// console.log(typeof num);
+// String - 'a', 'alphabet'
+// var str = "This is a string";
+// console.log(typeof str);
+// var char = "a";
+//Object (key - value pair)
+// var obj = {
+//   name: "John Doe",
+//   age: 78,
+// };
+
+// console.log(typeof obj);
+
+// Array
+// var arr = [7, 8, 9, 0];
+
+// console.log(typeof arr);
+
+// Undefined
+// var und;
+// console.log(typeof und);
+
+// Boolean (true/false)
+// var bool1 = true;
+// var bool2 = false;
+
+// Functions
+// var num1 = 9;
+// var num2 = 10;
+// console.log(num1 + num2);
+
+// Declare function
+// function add(num1, num2) {
+//   console.log(num1 + num2);
+// }
+
+// add(4, 7);
+// add(8, 9);
+
+// Object Deep Dive
